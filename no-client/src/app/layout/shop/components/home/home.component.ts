5a88a93a03e89d2f12f97e122eb06ea131d62670
Dang Mixx <dangmixx>
2021-09-28 23:54:05 +0700

    update card

--- no-client/src/app/layout/shop/components/home/home.component.ts
+++ no-client/src/app/layout/shop/components/home/home.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+    public listItem = [1, 2, 3, 4];
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
